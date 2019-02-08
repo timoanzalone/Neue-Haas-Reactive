3e55d53e931bfd9cc784350d44237aa3eb185b50
timoanzalone <timo.anzalone@gmail.com>
2019-02-08 11:56:08 +0100

    update score and credits

--- neue-haas.js
+++ neue-haas.js
@@ -6,7 +6,7 @@ document.addEventListener("mousemove", function(event) {
     const dx = div.offsetLeft + 50 - x
     const dy = div.offsetTop + 50 - y
     const dist = Math.sqrt(dx * dx + dy * dy)
-    const score = Math.exp(dist * -0.002)
+    const score = Math.exp(dist * -0.004)
 
     div.style.transform = "scale(" + score +")"
     div.style.fontWeight= 100 + (100 * Math.round(5 * score))
