5700f91d666574a338222cc879c8770348687226
timoanzalone <timo.anzalone@gmail.com>
2019-02-08 10:09:30 +0100

    Commit experiment to Github

---
+++ neue-haas.js
@@ -0,0 +1,14 @@
+document.addEventListener("mousemove", function(event) {
+  const x = event.pageX
+  const y = event.pageY
+
+  document.querySelectorAll(".reactive div").forEach(div => {
+    const dx = div.offsetLeft + 50 - x
+    const dy = div.offsetTop + 50 - y
+    const dist = Math.sqrt(dx * dx + dy * dy)
+    const score = Math.exp(dist * -0.002)
+
+    div.style.transform = "scale(" + score +")"
+    div.style.fontWeight= 100 + (100 * Math.round(5 * score))
+  })
+})
